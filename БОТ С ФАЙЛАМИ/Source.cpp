7d1a56f1f411b772c45d8fc3756a506cab172689
ZyWenn31 <topchic525@gmail.com>
2024-02-28 12:55:57 +0300

    Добавьте файлы проекта.

---
+++ БОТ С ФАЙЛАМИ/Source.cpp
@@ -0,0 +1,17 @@
+#define SQLITECPP_COMPILE_DLL
+#define _CRT_SECURE_NO_WARNINGS
+#include "tgbot/tgbot.h"
+#include "SQLiteCpp/SQLiteCpp.h"
+#include "curl/curl.h"
+#include "json/json.h"
+#include <vector>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+
+}
